37f908aaa25ed35aa925f00353e128a1558ca1f7
Kousuke0320 <b1015013@gmail.com>
2018-05-02 14:04:51 +0900

    Project's Initial Commit

---
+++ www/JS/back.js
@@ -0,0 +1,13 @@
+//b2ad1ad2eef5d227d63f4361e690020186a1f51d39428e14b2320d145e3ff1e0
+
+
+//2b9f2bb9895ba7cc701f432a220edd739374f757ccadb49f6e246c78931871bb
+
+var keepInf = function(){
+var username = document.getElementById('userName').value;
+  if (username == 'bob') {
+    alert('Congratulations!');
+  } else {
+    alert('Incorrect username or password.');
+  }
+};
