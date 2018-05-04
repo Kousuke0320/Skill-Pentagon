0043e6df6296f1cd4c1b3068b4e6a2d7fcb81cde
Kousuke0320 <b1015013@gmail.com>
2018-05-04 16:52:27 +0900

    mobile backend ととのコネクトに成功

--- www/JS/back.js
+++ www/JS/back.js
@@ -1,13 +1,26 @@
-//b2ad1ad2eef5d227d63f4361e690020186a1f51d39428e14b2320d145e3ff1e0
+var appKey    = "b2ad1ad2eef5d227d63f4361e690020186a1f51d39428e14b2320d145e3ff1e0";
+var clientKey = "2b9f2bb9895ba7cc701f432a220edd739374f757ccadb49f6e246c78931871bb";
+
+var ncmb = new NCMB(appKey, clientKey);
+// 保存先クラスの作成
+var TestClass = ncmb.DataStore("TestClass");
+
+// 保存先クラスのインスタンスを生成
+var testClass = new TestClass();
+
+// 値を設定と保存
+testClass.set("message", "Hello, NCMB!")
+         .save()
+         .then(function(object){
+             // 保存に成功した場合の処理
+
+         })
+         .catch(function(err){
+             // 保存に失敗した場合の処理
+             alert("tintin");
+         });
+
+ 
 
 
-//2b9f2bb9895ba7cc701f432a220edd739374f757ccadb49f6e246c78931871bb
 
-var keepInf = function(){
-var username = document.getElementById('userName').value;
-  if (username == 'bob') {
-    alert('Congratulations!');
-  } else {
-    alert('Incorrect username or password.');
-  }
-};
--- www/JS/main.js
+++ www/JS/main.js
@@ -1,2 +1,37 @@
 // This is a JavaScript file
 
+
+var keepInf = function(){
+var username = document.getElementById('userName').value;
+  if (username == 'bob') {
+    alert('Congratulations!');
+  } else {
+    alert('Incorrect username or password.');
+  }
+};
+
+
+
+function getPicLib()
+        {
+            var option = {
+                quality: 50,
+                sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+                correntOrientation: true,
+                // destinationType: Camera.DestinationType.DATA_URL
+                destinationType: Camera.DestinationType.FILE_URI
+            };
+            navigator.camera.getPicture(onSuccess, onFail, option);
+        }
+
+        function onFail()
+        {
+            console.log("写真を取得できませんでした")
+        }
+
+        function onSuccess(imageURI)
+        {
+            
+         
+        }
+
