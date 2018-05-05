0229cbdbc5334aee86fd08a429a313dd6dfb3d02
Kousuke0320 <b1015013@gmail.com>
2018-05-05 13:47:48 +0900

    ログイン画面実装成功

--- www/JS/back.js
+++ www/JS/back.js
@@ -2,23 +2,42 @@ var appKey    = "b2ad1ad2eef5d227d63f4361e690020186a1f51d39428e14b2320d145e3ff1e
 var clientKey = "2b9f2bb9895ba7cc701f432a220edd739374f757ccadb49f6e246c78931871bb";
 
 var ncmb = new NCMB(appKey, clientKey);
-// 保存先クラスの作成
-var TestClass = ncmb.DataStore("TestClass");
 
-// 保存先クラスのインスタンスを生成
-var testClass = new TestClass();
+var currentLoginUser; //現在ログイン中ユーザー
 
-// 値を設定と保存
-testClass.set("message", "Hello, NCMB!")
-         .save()
-         .then(function(object){
-             // 保存に成功した場合の処理
+function keepInf(){ 
 
-         })
-         .catch(function(err){
-             // 保存に失敗した場合の処理
-             alert("tintin");
-         });
+var username = document.getElementById('userName').value;
+var password = document.getElementById('password').value;
+
+//　Userインスタンスの生成
+var user = new ncmb.User();
+user.set("userName", username)
+    .set("password", password);
+    
+
+ncmb.User.login(username, password)
+    .then(function(date){
+      // ログイン後処理
+    currentLoginUser = ncmb.User.getCurrentUser();
+   window.location.href = 'index2.html'; // 通常の遷移
+    })
+    .catch(function(err){
+      // エラー処理
+      
+      user.signUpByAccount()
+    .then(function(){
+      // 登録後処理
+      currentLoginUser = ncmb.User.getCurrentUser();
+      alert("seikou");
+      window.location.href = 'index2.html'; // 通常の遷移
+    })
+    .catch(function(err){
+      // エラー処理
+      alert(err);
+    });
+    });
+}
 
  
 
--- www/JS/main.js
+++ www/JS/main.js
@@ -1,17 +1,3 @@
-// This is a JavaScript file
-
-
-var keepInf = function(){
-var username = document.getElementById('userName').value;
-  if (username == 'bob') {
-    alert('Congratulations!');
-  } else {
-    alert('Incorrect username or password.');
-  }
-};
-
-
-
 function getPicLib()
         {
             var option = {
@@ -31,7 +17,6 @@ function getPicLib()
 
         function onSuccess(imageURI)
         {
-            
-         
+            var toppic = document.getElementById("topPic");
+            toppic.src = imageURI;
         }
-
